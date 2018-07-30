ada12dbe0e8dafedb5d3535f2d8fce4ca9a61b33
Vijai Baskar <jsathyan@hawk.iit.edu>
2018-07-29 22:10:41 -0500

    Adding settings screen to app

--- app/src/main/java/com/example/android/newsapp/NewsActivity.java
+++ app/src/main/java/com/example/android/newsapp/NewsActivity.java
@@ -5,11 +5,16 @@ import android.app.LoaderManager.LoaderCallbacks;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -18,13 +23,13 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class NewsActivity extends AppCompatActivity implements LoaderCallbacks<List<News>>{
+public class NewsActivity extends AppCompatActivity implements LoaderCallbacks<List<News>> {
 
     private NewsAdapter mAdapter;
     private TextView mEmptyStateTextView;
     private static final int NEWS_LOADER_ID = 1;
 
-    private static final String GUAR_REQUEST_URL = "https://content.guardianapis.com/search?show-tags=contributor&api-key=fbfed1fc-1c50-40b2-8392-d4a19218006c";
+    private static final String GUAR_REQUEST_URL = "https://content.guardianapis.com/search?show-tags=contributor";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,33 +71,66 @@ public class NewsActivity extends AppCompatActivity implements LoaderCallbacks<L
         loaderManager.initLoader(NEWS_LOADER_ID, null, this);
     }
 
-        @Override
-        public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
-            return new NewsLoader(this, GUAR_REQUEST_URL);
-        }
+    @Override
+    public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
 
-        @Override
-        public void onLoadFinished(Loader<List<News>> loader, List<News> newsStories) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
-            View loadingIndicator = findViewById(R.id.loading_spinner);
-            loadingIndicator.setVisibility(View.GONE);
-            mEmptyStateTextView.setText(R.string.no_data_available);
+        String apiKey = getString(R.string.api_key);
+        String orderBy = sharedPreferences.getString(getString(R.string.settings_order_by_key), getString(R.string.settings_order_by_default));
+        String numberOfStories = sharedPreferences.getString(getString(R.string.settings_number_of_stories_key), getString(R.string.settings_number_of_stories_default));
 
-            mAdapter.clear();
+        Uri baseUri = Uri.parse(GUAR_REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
 
+        uriBuilder.appendQueryParameter("order-by", orderBy);
+        uriBuilder.appendQueryParameter("page-size", numberOfStories);
+        uriBuilder.appendQueryParameter("api-key", apiKey);
 
-            if (newsStories != null && !newsStories.isEmpty()){
-                mAdapter.addAll(newsStories);
-            }
-
-
-        }
-
-        @Override
-        public void onLoaderReset(Loader<List<News>> loader) {
-            // TODO: Loader reset, so we can clear out our existing data.
-            mAdapter.clear();
-
-        }
+        Log.i("NewsActivity.java", uriBuilder.toString());
+        return new NewsLoader(this, uriBuilder.toString());
     }
+
+    @Override
+    public void onLoadFinished(Loader<List<News>> loader, List<News> newsStories) {
+
+        View loadingIndicator = findViewById(R.id.loading_spinner);
+        loadingIndicator.setVisibility(View.GONE);
+        mEmptyStateTextView.setText(R.string.no_data_available);
+
+        mAdapter.clear();
+
+
+        if (newsStories != null && !newsStories.isEmpty()) {
+            mAdapter.addAll(newsStories);
+        }
+
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<News>> loader) {
+        mAdapter.clear();
+
+    }
+
+    @Override
+    // This method initialize the contents of the Activity's options menu.
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the Options Menu we specified in XML
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
 
